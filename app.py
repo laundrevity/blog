44c34207970645bdf814a667c5a668e2e96a3465
laundrevity <mahany25@gmail.com>
2024-01-16 20:08:48 -0500

    auto generate article dropdown options using context processor

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask, render_template
 import markdown2
+import os
 
 
 app = Flask(__name__)
+
+
+def get_article_names():
+    articles = []
+    for filename in os.listdir("articles/"):
+        if filename.endswith(".md"):
+            article_title = filename[:-3]  # remove the .md extension
+            article_title = article_title[0].upper() + article_title[1:]
+            article_title = article_title.replace("_", " ")
+            articles.append(article_title)
+    return articles
+
+
+@app.context_processor
+def inject_articles():
+    return {"articles": get_article_names()}
 
 
 @app.route("/", methods=["GET"])
@@ -12,10 +29,11 @@ def index():
 
 @app.route("/article/<article_name>", methods=["GET"])
 def article(article_name: str):
-    with open(f"articles/{article_name}.md", "r") as f:
+    filename = article_name.lower().replace(" ", "_")
+
+    with open(f"articles/{filename}.md", "r") as f:
         content = markdown2.markdown(f.read())
 
-    print(f"{content=}")
     return render_template("article.html", content=content)
 
 
