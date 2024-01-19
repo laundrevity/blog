031e77c2ea6336efa2912cb6554a49129be8e914
laundrevity <mahany25@gmail.com>
2024-01-19 14:53:56 -0500

    make it gunicorn killable

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ mkdir -p logs
 gunicorn app:app \
     --workers 4 \
     --bind 0.0.0.0:8000 \
-    # --log-file logs/gunicorn.log \
-    --log-level DEBUG
+    --log-level debug \
+    --pid gunicorn.pid
