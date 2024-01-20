888c6e4e735609d70a14969867dc48a3b2210770
laundrevity <mahany25@gmail.com>
2024-01-19 22:21:30 -0500

    static folder for images

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 import os
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1
 )
--- articles/data_serialization.md
+++ articles/data_serialization.md
@@ -1,1 +1,3 @@
 WIP, <a href="https://github.com/laundrevity/xparse">repo is here</a>
+
+![Alt text](/static/tikz/diagram.png)
