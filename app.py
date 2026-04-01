745e1bdaf2cbc09c94d164c11ceba31629a880f1
Conor Mahany <mahany25@gmail.com>
2026-04-01 17:15:53 -0400

    add tables to markdown extras

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_folder="static")
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1
 )
-markdowner = Markdown(extras=["fenced-code-blocks"])
+markdowner = Markdown(extras=["fenced-code-blocks", "tables"])
 
 # Configure Rotating File Handler
 os.makedirs("logs", exist_ok=True)
